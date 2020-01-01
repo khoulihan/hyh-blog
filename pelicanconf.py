24c08692f0c5758d3be8cfb78537c33584fd0e6c
Kevin Houlihan <kevin@crimsoncookie.com>
2020-01-01 22:58:03 +0100

    Changed the breadcrumbs to look like a tab jutting out of the header.

--- pelicanconf.py
+++ pelicanconf.py
@@ -51,7 +51,7 @@ MENUITEMS = (('Home', ''),
             ('Categories', 'categories.html'),
             ('Tags', 'tags.html'),)
 
-DEFAULT_PAGINATION = 1
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
